0027cc2db75dfd69ac8b2d0bfd2ccbe73d1d0443
DavidQATraining <david.j.mccartney@gmail.com>
2020-06-30 14:46:28 +0100

    Throw dice module, and file using created module

---
+++ Day1/dice.py
@@ -0,0 +1,6 @@
+import random
+
+
+def throwdice():
+    num1, num2 = random.randint(1, 7), random.randint(1, 7)
+    return num1, num2
---
+++ Day1/useDiceModule.py
@@ -0,0 +1,3 @@
+from Day1 import dice
+
+print(dice.throwdice())
