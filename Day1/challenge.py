b1a8891dd1107fc47e4190210ca0296bbeacc86f
DavidQATraining <david.j.mccartney@gmail.com>
2020-07-01 09:31:26 +0100

    new folder > initfile > filesTask

---
+++ Day1/challenge.py
@@ -0,0 +1,28 @@
+# 169179692512835000
+
+import math
+
+def calBase(num):
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def height_tetrahedron(num):
+#     #volume = (num ** 3 / (6 * math.sqrt(2)))
+#     h = math.sqrt(6) / 2 * num
+#
+#     return round(h, 2)
+#
+#
+# print(height_tetrahedron(169179692512835000))
+# #h=sqr(3)/2*a
+# #H=sqr(6)/3*a
