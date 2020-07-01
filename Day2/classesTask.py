5c717107a128b8508f65f14b9f7b5f76773e7f49
DavidQATraining <david.j.mccartney@gmail.com>
2020-07-01 14:39:11 +0100

    classTask solutions update

--- Day2/classesTask.py
+++ Day2/classesTask.py
@@ -1,22 +1,24 @@
 class Student:
-    class_ = "Student"
+    group = "Student"
 
     # constructor method
-    def __init__(self, name, age, class_):
+    def __init__(self, name, age, group):
         self.name = name
         self.age = age
-        self.class_ = class_
+        self.type = group
 
     # class function
     def getAvgScore(self, score1, score2, score3):
         return "avgScore is " + str((score1+score2+score3) / 3)
 
 
-# creating 3 students
+# creating 4 entities
 Dave = Student("Dave", 30, "Student")
 Tadas = Student("Tadas", 27, "Lecturer")
 Paul = Student("Paul", 27, "Student")
+Robert = Student("Robert", 27, "Student")
 
-print(getattr(Dave, "name"), getattr(Dave, "class_"))
-print(getattr(Tadas, "name"), getattr(Tadas, "class_"))
+print(getattr(Dave, "name"), getattr(Dave, "group"))
+print(getattr(Tadas, "name"), getattr(Tadas, "group"))
+print(getattr(Robert, "name"), getattr(Robert, "group"))
 print(getattr(Dave, "name") + "'s " + Dave.getAvgScore(98, 94, 90))
