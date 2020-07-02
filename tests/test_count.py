c77c1861906be4e52690f6fc4ca6d414b6575b2f
DavidQATraining <david.j.mccartney@gmail.com>
2020-07-02 09:41:33 +0100

    python-testing folder added to do tasks

--- tests/test_count.py
+++ tests/test_count.py
@@ -1,10 +1,10 @@
-import pytest
+from unittest import TestCase
 from Day2 import count
 
 
-def test_count_zeros():
-    assert count.count([0, 0, 0], 0) == 3
+class Test(TestCase):
+    def test_count_zeros(self):
+        assert count.count([0, 0, 0], 0) == 3
 
-
-def test_count_string():
-    assert count.count(["a", "a", "a"], "a") == 3
+    def test_count_string(self):
+        assert count.count(["a", "a", "a"], "a") == 3
